c45f8698325c031f5efdf6f674317d571cceafb0
Wesley Berry <wesleyberry52@gmail.com>
2019-06-27 18:23:16 -0500

    Updated React App. Workflow: AWS Amplify Console

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's not Learn React
         </a>
       </header>
     </div>
